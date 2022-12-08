8ded1dc36018b0f9fb39f61588d78eacea876361
rootChagest <softwarechristian1@gmail.com>
2022-12-08 18:54:26 +0100

    frontend...

--- src/features/api.js
+++ src/features/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const url = "http://localhost:8000/api/v1";
 */
 // Production
-export const url = "https://chagest-eshop.onrender.com/api/v1/";
+export const url = "https://chagest-eshop.onrender.com/api/v1";
 
 const token = localStorage.getItem("token");
 
